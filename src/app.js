4c030871b478d78aa7f809c95bac0554ecb020af
GORKEM-KORKUT <89797544+GORKEM-KORKUT@users.noreply.github.com>
2021-10-19 00:18:50 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,46 @@
+console.log("Merhaba Kodlama.io")
+//HS type safe değildir
+let dolarBugun = 9.30
+
+let dolarDun = 9.20
+dolarDun = "9.20"
+{
+    let dolarDun = 9.10
+}
+
+
+console.log(dolarDun)
+
+const euroDun = 15
+
+console.log(euroDun)
+
+
+
+//array
+//camelCasing
+//PascalCasing
+let kk = ["Konut kredisi","Emlak konut kredisi","kamu konut kredisi"]
+//react
+console.log("<ul>")
+for(let i = 0; i < kk.length; i++){
+    console.log("<li>"+kk[i]+"</li>")
+
+}
+
+console.log("</ul>")
+
+{/*<ul>
+    <li>Konut kredisi</li>
+    <li>Emlak konut kredisi</li>
+    <li>kamu konut kredisi</li>
+
+</ul>*/}
+
+
+console.log(kk)
+
+
+
+
+
